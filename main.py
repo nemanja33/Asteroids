d32d4067105d55f903bbaf90b0bd7f2ede67f712
nemanja33 <djokic.nemanja97@gmail.com>
2025-05-07 08:26:10 +0200

    finished player

--- main.py
+++ main.py
@@ -1,16 +1,43 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
+    # player
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    new_player = Player(x, y)
+
+    # game
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    # game loop
     while (True):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
+        updatable.update(dt)
+
+        # screen fill
         screen.fill("black")
+
+        # player
+        for item in drawable:
+            item.draw(screen)
+
+        # idk
         pygame.display.flip()
+
+        # fps
+        clock.tick(60)
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
